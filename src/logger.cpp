6470446f03e7a1d2d8fdc23ef78ccde8339ad522
leegao <lg342@cornell.edu>
2026-05-27 00:46:14 +0800

    Fix logger initialization never being called

--- src/logger.cpp
+++ src/logger.cpp
@@ -11,7 +11,10 @@ namespace Logger {
 	
 	uint64_t bcn_layer_log_mask;
 
+	void init();
+
 	static unsigned long long get_debug_flag(const char *option) {
+	    Logger::init();
     	int index = 0;
 
         while (!bcn_layer_log_options[index].name.empty()) {
@@ -49,6 +52,11 @@ namespace Logger {
 	}
 
 	void init() {
+	    static bool initialized = false;
+	    if (initialized)
+	        return;
+	    initialized = true;
+		
 		char *bcn_layer_log_env = std::getenv("BCN_LAYER_LOG_LEVEL");
 		if (!bcn_layer_log_env) {
 			bcn_layer_log_mask = 0;
